7e19123a3663bc2564f69c80a60f334f09663d74
sumansingh6degreeit <99945059+sumansingh6degreeit@users.noreply.github.com>
2022-03-29 16:09:36 +0530

    code update

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import {CONNECTION_URL } from "./config/index.js";
  import bodyParser from 'body-parser'
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", routes);
 
 const PORT = process.env.PORT || 3000;
