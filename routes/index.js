3e52b168fbe55238e5ba2057de729fced5ef88b6
sumansingh6degreeit <99945059+sumansingh6degreeit@users.noreply.github.com>
2022-03-29 23:01:52 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { insertData } from "../controllers/index.js";
 const router = express.Router();
-
 router.post("/insertdata", insertData);
 
 export default router;
